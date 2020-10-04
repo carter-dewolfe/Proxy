3232e44588c273e0d91f40ba0c343d241e845492
Carter DeWolfe <48815767+carter-dewolfe@users.noreply.github.com>
2020-10-04 16:53:54 -0500

    Final touches!

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,17 @@
 import java.io.*;
 import java.net.*;
 
+/*
+Resources:
+https://github.com/stefano-lupo/Java-Proxy-Server
+https://github.com/Tyler-Losinski/Chaka-Proxy
+https://github.com/pattibabie/ProxyServer-Java
+https://github.com/sundychenD/HTTPProxy
+https://github.com/adamfisk/LittleProxy
+
+Note: these were used as assistance to help create my own proxy server
+ */
+
 public class Server {
     public static void main(String[] args) throws IOException {
         ServerSocket socket = new ServerSocket(5678);
